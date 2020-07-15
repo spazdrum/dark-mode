467301c1ed80325f7069de25a685091fb7e890bf
spazdrum <spazdrum@gmail.com>
2020-07-14 23:48:52 -0400

    finished useLocalStorage

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -6,5 +6,10 @@ export const useLocalStorage = (key, initialValue) => {
     return item ? JSON.parse(item) : initialValue;
   });
 
-  return [storedValue];
+  const setValue = (value) => {
+    setStoredValue(value);
+    window.localStorage.setItem(key, JSON.stringify(value));
+  };
+
+  return [storedValue, setValue];
 };
